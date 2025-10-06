663844da9ec48404f85fce941fce6d291cfeb6ab
blackbydefault <119285013+blackbydefault@users.noreply.github.com>
2025-10-06 16:43:27 -0400

    Add: Implemented Coin Flip Count Program

--- CountFlips.java
+++ CountFlips.java
@@ -1,4 +1,22 @@
 public class CountFlips {
   public static void main(String[] args) {
+    int headsCount = 0;
+    int tailsCount = 0;
+    Coin flippableCoin = new Coin();
+
+    for(int z = 0; z < 100; z++)
+    {
+      flippableCoin.flip();
+      if (flippableCoin.toString() == "Heads"){
+        headsCount++;
+      }
+      else{
+        tailsCount++;
+      }
+    }
+    System.out.println("*Flipping a coin 100 times...*\n[HEADS]: " + headsCount + "\n[TAILS]: " + tailsCount);
+
+
+
   }
 }
