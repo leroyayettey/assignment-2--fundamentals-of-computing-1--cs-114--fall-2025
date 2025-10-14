9915580c8ab475f6503bd78c3c75d72acb32a3bf
blackbydefault <119285013+leroyayettey@users.noreply.github.com>
2025-10-14 19:58:52 -0400

    Add: Implemented Bottles of Beers Program

--- OneHundredBottlesOfBeer.java
+++ OneHundredBottlesOfBeer.java
@@ -1,4 +1,25 @@
+import java.util.Scanner;
+
 public class OneHundredBottlesOfBeer {
   public static void main(String[] args) {
+    Scanner userInput = new Scanner(System.in);
+    int numberOfVerses = 0;
+    int numberOfBeers = 100;
+
+    System.out.println("Enter the desired number of verses to print from the traveling song ' One Hundred Bottles of Beer. ' ");
+    int input = userInput.nextInt();
+
+    if (input > 100 || input < 1){
+      System.out.println("** INVALID INPUT **\nCHOSEN NUMBER OF VERSES MUST BE WITHIN THE RANGE (1 - 100) ");
+    }
+    else{
+      numberOfVerses = input;
+    }
+    for(int i = 0; i < numberOfVerses; i++){
+      System.out.println(numberOfBeers + " bottles of beer on the wall\n" + numberOfBeers + " bottles of beer\nIf one of those bottles should happen to fall\n" + (numberOfBeers--) + " bottles of beer on the wall\n");
+    }
+    userInput.close();
+    }
+
+
   }
-}
